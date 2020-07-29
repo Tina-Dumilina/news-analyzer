75853c60b840ca6b13ba282106c24359ccef1a0d
Tina Dumilina <tina.dumilina@gmail.com>
2020-07-29 22:24:09 +0300

    Added github section and swiper

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,1 +1,31 @@
+import 'swiper/swiper-bundle.css';
 import './style.css';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 16,
+  centeredSlides: true,
+
+  slideClass: 'swiper__slide',
+  slideNextClass: 'swiper__slide_next',	
+  slidePrevClass: 'swiper__slide_prev',
+  slideActiveClass: 'swiper__slide_active',
+  wrapperClass: 'swiper__wrapper',
+
+  pagination: {
+    el: '.swiper__pagination',
+    type: 'bullets',
+    clickable: true,
+    bulletClass: 'swiper__bullet',
+    bulletActiveClass: 'swiper__bullet_active'
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
